cc1c041b70f1ef2d37a852239093821ffc6456fb
Hanush M <136079234+mhanush@users.noreply.github.com>
2024-04-20 21:17:19 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,12 +5,7 @@ import * as firebase from "firebase";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCLA4vnLFaVow25ylLUkb57NhmnN6qk1vE",
-  authDomain: "fir-auth-65294.firebaseapp.com",
-  projectId: "fir-auth-65294",
-  storageBucket: "fir-auth-65294.appspot.com",
-  messagingSenderId: "105428798939",
-  appId: "1:105428798939:web:49a9375f32ea94d4dfebfe"
+
 };
 
 // Initialize Firebase
