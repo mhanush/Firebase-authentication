b093fc3bff44094f36006910395cf77295c27ab3
Hanush M <136079234+mhanush@users.noreply.github.com>
2024-04-21 11:13:39 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,21 +1,11 @@
-//Import the functions you need from the SDKs you need
-import * as firebase from "firebase";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 const firebaseConfig = {
 
 };
 
-// Initialize Firebase
-let app;
-if (firebase.apps.length === 0) {
-  app = firebase.initializeApp(firebaseConfig);
-} else {
-  app = firebase.app()
-}
-
-const auth = firebase.auth()
-
-export { auth };
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app); 
+export { auth, createUserWithEmailAndPassword, signInWithEmailAndPassword };
