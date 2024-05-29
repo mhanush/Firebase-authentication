ce733fc5e1a3298b353c7d84f93e744f003ea585
Hanush M <136079234+mhanush@users.noreply.github.com>
2024-05-29 18:47:47 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,13 +4,13 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAlCkvLu-06IwsgpOUsgb9ySF3jzEv1voE",
-  authDomain: "hackathon-cse-4e613.firebaseapp.com",
-  projectId: "hackathon-cse-4e613",
-  storageBucket: "hackathon-cse-4e613.appspot.com",
-  messagingSenderId: "830373186239",
-  appId: "1:830373186239:web:f3dcbaf6d9256e9bb4d40a",
-  measurementId: "G-6EMNH0J65W"
+  apiKey: "AIzaSyAlCkv06IwsgpOUsgb9ySF3jzEv1voE",
+  authDomain: "hacka-4e613.firebaseapp.com",
+  projectId: "hackathe613",
+  storageBucket: "hackapspot.com",
+  messagingSenderId: "83036239",
+  appId: "1:8303731[9:wd9256e9bb4d40a",
+  measurementId: "G-H0J65W"
 };
 
 const app = initializeApp(firebaseConfig);
